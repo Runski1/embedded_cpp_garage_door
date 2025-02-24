46f949d730e126f150cd5076f070d3042e00ee44
Aaro Saila <aaros.saila@gmail.com>
2025-02-24 18:51:27 +0200

    Streamlined test method

--- src/hardware_classes/StepperMotor.cpp
+++ src/hardware_classes/StepperMotor.cpp
@@ -35,30 +35,59 @@ void StepperMotor::step_left() {
   set_phase((phase - 1) % 8);
 }
 
-// For testing
 
-// int main() {
-//   StepperMotor motor;
-//
-//   stdio_init_all();
-//
-//   gpio_init(7);
-//   gpio_pull_up(7);
-//   gpio_set_dir(7, GPIO_IN);
-//   gpio_set_inover(7, GPIO_OVERRIDE_INVERT);
-//   gpio_init(9);
-//   gpio_pull_up(9);
-//   gpio_set_dir(9, GPIO_IN);
-//   gpio_set_inover(9, GPIO_OVERRIDE_INVERT);
-//
-//   while (true) {
-//     if (gpio_get(7)) {
-//       printf("right");
-//       motor.step_right();
-//     } else if (gpio_get(9)) {
-//       printf("left");
-//       motor.step_left();
-//     }
-//     sleep_ms(10);
-//   }
-// }
+#ifdef MOTOR_TEST
+
+#include <iostream>
+#include "pico/time.h"
+#include "pico/util/queue.h"
+
+#include "../irq/irq.h"
+#include "../pins.h"
+
+void StepperMotor::test() {
+  std::cout << "In StepperMotor::test" << std::endl;
+
+  StepperMotor motor;
+  int sleep_time_ms = 1000;
+
+  enum state {LEFT, RIGHT, STOP};
+  state state = STOP;
+
+  const int pins[] = {BTN_0, BTN_1, BTN_2};
+  for (auto pin : pins) {
+    gpio_init(pin);
+    gpio_pull_up(pin);
+    gpio_set_dir(pin, GPIO_IN);
+    gpio_set_inover(pin, GPIO_OVERRIDE_INVERT);
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_RISE, true);
+  }
+
+  while (true) {
+    int x;
+    while (queue_try_remove(&irq_queue, &x)) {
+      switch (x) {
+        case PRESS_0: state = RIGHT; break;
+        case PRESS_1: state = STOP; break;
+        case PRESS_2: state = LEFT; break;
+      }
+    }
+
+    switch (state) {
+      case RIGHT:
+        // printf("right");
+        motor.step_right();
+        break;
+      case LEFT:
+        // printf("left");
+        motor.step_left();
+        break;
+      case STOP:
+        break;
+    }
+
+    sleep_us(WAIT_TIME_US);
+  }
+}
+
+#endif
