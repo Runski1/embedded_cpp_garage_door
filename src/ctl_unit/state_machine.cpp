22024430fbd61630f0242524c4cfa04579eee404
BurningStratus <pavel.shishkin@protonmail.com>
2025-02-24 15:01:25 +0200

    more files

--- src/ctl_unit/state_machine.cpp
+++ src/ctl_unit/state_machine.cpp
@@ -1,31 +1,88 @@
 
 #include "state_machine.h"
 
+
 StateMachine::StateMachine()
-{
-}
+{}
 
-
-StateMachine::StateMachine(int st_in)
-{
-}
-
+// returns status of the door
 int StateMachine::operator()()
+{}
+
+// returns the status of the door
+int StateMachine::getStatus(void)
+{}
+
+/*
+// set the status of the door
+void StateMachine::operator()(int)
+{}
+
+
+// set the status of the door
+void StateMachine::setStatus(int)
+{}
+*/
+/*
+// advance the state to the next one(0→1→2→3→0) 
+int StateMachine::advance()
 {
+    switch (this->state)
+    {
+        // closed
+        case (0):
+            ++this->state;  // set to "opening"
+            break;
+        // opening
+        case (1):
+            ++this->state;  // set to "open"
+            break;
+        // open
+        case (2):
+            ++this->state;  // set to "closing"
+            break;
+        // closing
+        case (3):
+            this->state=0;  // set to "closed"
+            break;
+        default:
+            this->state=4; // set to "broken"
+    }
+    return (this->state);
+}
+*/
+
+// check the signal flag and if it is set, then do something 
+void StateMachine::operate(void)
+{
+    //if (signal) signal=!signal; // to prevent changing states directly
+    //else action();  // action
+
 }
 
-void StateMachine::operator()(int st_update)
+// set a flag for doing stuff
+void StateMachine::signal(void)
 {
+    signal=true;
 }
 
-int StateMachine::getState()
+void StateMachine::action(void)
 {
-}
+    signal=!signal; // reset signal
 
-void StateMachine::setState(int st_update)
-{
-}
+    // add check for calibration
+    /*
+    if (state_mvdir != 0 && SPEED == 0 )   // add speed here
+    {
+    }
+    */
+    //using bitwise to avoid ifelse hell and to increase unnecessary complexity
+    if (state_door <= 2 ) state_door=(1<<2);    // set state to 4(moving)
+    
+    
 
-void StateMachine::operate()
-{
+    if (state_door == 3)
+        // DOOR_MOVE ( direction )
+    else if (state_door)
+
 }
--- src/ctl_unit/state_machine.h
+++ src/ctl_unit/state_machine.h
@@ -1,22 +1,6 @@
 
+#include "pico/stdlib.h"
 #ifndef STATEMACHINE_H_
-
-/* 
-class Controller {
-    public:
-        int RM; 
-    private:
-        int RM0;
-};
-
- * the highest class. It would contain methods to do stuff with states 
- * > Do something based on info which it would get from state.
- *      Maybe smart pointer?
- *
- * State would be an object to set and change the state of the system 
- * > contain info about state
- *
- * */
 
 /*
 
@@ -26,9 +10,7 @@ class Controller {
 > Calibration state: Calibrated/Not calibrated
 
 >> Local operation: 
-
-    Pressing SW1 has following functionality:
-
+Pressing SW1 has following functionality:
 o Door is closed >> door starts to open
 o Door open >> door starts to close
 o Door is current opening or closing → door stops
@@ -39,23 +21,33 @@ opposite direction (stopped during opening→close and vice versa)
 
 class StateMachine {
 public:
-    int operator()();
-    void operator()(int);
-    int operator++(StateMachine &);
-    int operator--(StateMachine &);
-    int getState(void) const;
-    void setState(int);
-    void operate(void);
-
     StateMachine();
-    StateMachine(int);
+    //StateMachine(int);
     StateMachine(StateMachine &) = delete; // do not copy
 
+    int operator()() const;     // returns status of the door
+    void operator()(int);       // set the status of the door
+
+    bool getStatus(void) const;  // returns the status of the door
+    void setStatus(bool);        // set the status of the door
+
+    int getDirection();
+    void setDirection();
+
+    bool getError();
+    void setError();
+
+    void operate(void); // do stuff based on states
+    void signal(void);  // set a flag for doing stuff
+    void action(void);
+
 private:
-    // 0: closed, 1:opening, 2:closing, 3: stuck, 4: open 
-    int state_door;
+    int state_door;         // 1:closed, 2:open, 4=moving, 8=still
+    bool state_mvdir;       // 0=down, 1=up, 
     bool status_calibrated; // door status
-    bool status_error;
+    bool status_error;      // error
+
+    bool signal; 
 };
 
 #endif
