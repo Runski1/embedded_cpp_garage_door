882f2e08918ecd4c34242db4c596e1151ed7e27f
Matias Ruonala <placeholder@asdf.com>
2025-02-25 11:42:43 +0200

    QOS1&2 stuff removed from mqtt test

--- src/network/network_testbench.cpp
+++ src/network/network_testbench.cpp
@@ -1,42 +1,26 @@
-#include <stdio.h>
-#include <string.h>
-#include <cmath>
+#include "hardware/timer.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
-#include "hardware/timer.h"
-#include "uart/PicoUart.h"
 #include <cstring>
+#include <stdio.h>
+#include <string.h>
 
-#include "IPStack.h"
 #include "Countdown.h"
+#include "IPStack.h"
 #include "MQTTClient.h"
-
-// We are using pins 0 and 1, but see the GPIO function select table in the
-// datasheet for information on which other pins can be used.
-#if 0
-#define UART_NR 0
-#define UART_TX_PIN 0
-#define UART_RX_PIN 1
-#else
-#define UART_NR 1
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-#endif
 
 #define BAUD_RATE 9600
 #define STOP_BITS 1 // for simulator
-//#define STOP_BITS 2 // for real system
+// #define STOP_BITS 2 // for real system
 
 #define USE_MQTT
-
-
 
 void messageArrived(MQTT::MessageData &md) {
     MQTT::Message &message = md.message;
 
     printf("Message arrived: qos %d, retained %d, dup %d, packetid %d\n",
            message.qos, message.retained, message.dup, message.id);
-    printf("Payload %s\n", (char *) message.payload);
+    printf("Payload %s\n", (char *)message.payload);
 }
 
 static const char *topic = "test-topic";
@@ -59,22 +43,22 @@ int main() {
 
     printf("\nBoot\n");
 
-
-#ifdef USE_MQTT
-    //IPStack ipstack("SSID", "PASSWORD"); // example
-    //IPStack ipstack("KME662", "SmartIot"); // example
+    // IPStack ipstack("SSID", "PASSWORD"); // example
+    // IPStack ipstack("KME662", "SmartIot"); // example
     IPStack ipstack("free_wifi", "r0t4nk0l0"); // example
     auto client = MQTT::Client<IPStack, Countdown>(ipstack);
 
+    // lwIP error codes ${PICO_SDK_PATH}/lib/lwip/src/include/lwip/err.h
     int rc = ipstack.connect("192.168.101.100", 1883);
     if (rc != 1) {
+        // TODO add rc translator
         printf("rc from TCP connect is %d\n", rc);
     }
 
     printf("MQTT connecting\n");
     MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
     data.MQTTVersion = 3;
-    data.clientID.cstring = (char *) "PicoW-sample";
+    data.clientID.cstring = (char *)"PicoW-sample";
     rc = client.connect(data);
     if (rc != 0) {
         printf("rc from MQTT connect is %d\n", rc);
@@ -84,7 +68,8 @@ int main() {
     }
     printf("MQTT connected\n");
 
-    // We subscribe QoS2. Messages sent with lower QoS will be delivered using the QoS they were sent with
+    //MQTT return codes
+//enum returnCode { BUFFER_OVERFLOW = -2, FAILURE = -1, SUCCESS = 0 };
     rc = client.subscribe(topic, MQTT::QOS2, messageArrived);
     if (rc != 0) {
         printf("rc from MQTT subscribe is %d\n", rc);
@@ -94,11 +79,8 @@ int main() {
     auto mqtt_send = make_timeout_time_ms(2000);
     int mqtt_qos = 0;
     int msg_count = 0;
-#endif
-
 
     while (true) {
-#ifdef USE_MQTT
         if (time_reached(mqtt_send)) {
             mqtt_send = delayed_by_ms(mqtt_send, 2000);
             if (!client.isConnected()) {
@@ -107,59 +89,25 @@ int main() {
                 if (rc != 0) {
                     printf("rc from MQTT connect is %d\n", rc);
                 }
-
             }
             char buf[100];
             int rc = 0;
             MQTT::Message message;
             message.retained = false;
             message.dup = false;
-            message.payload = (void*) buf;
+            message.payload = (void *)buf;
             printf("payload len: %d\n", message.payloadlen);
-            switch (mqtt_qos) {
-                case 0:
-                    // Send and receive QoS 0 message
-                    sprintf(buf, "Msg nr: %d QoS 0 message", ++msg_count);
-                    printf("%s\n", buf);
-                    message.qos = MQTT::QOS0;
-                    message.payloadlen = strlen(buf) + 1;
-                    rc = client.publish(topic, message);
-                    printf("Publish rc=%d\n", rc);
-                    ++mqtt_qos;
-                    break;
-                case 1:
-                    // Send and receive QoS 1 message
-                    sprintf(buf, "Msg nr: %d QoS 1 message", ++msg_count);
-                    printf("%s\n", buf);
-                    message.qos = MQTT::QOS1;
-                    message.payloadlen = strlen(buf) + 1;
-                    rc = client.publish(topic, message);
-                    printf("Publish rc=%d\n", rc);
-                    ++mqtt_qos;
-                    break;
-#if MQTTCLIENT_QOS2
-                    case 2:
-                        // Send and receive QoS 2 message
-                        sprintf(buf, "Msg nr: %d QoS 2 message", ++msg_count);
-                        printf("%s\n", buf);
-                        message.qos = MQTT::QOS2;
-                        message.payloadlen = strlen(buf) + 1;
-                        rc = client.publish(topic, message);
-                        printf("Publish rc=%d\n", rc);
-                        ++mqtt_qos;
-                        break;
-#endif
-                default:
-                    mqtt_qos = 0;
-                    break;
-            }
-        }
 
-        cyw43_arch_poll(); // obsolete? - see below
-        client.yield(100); // socket that client uses calls cyw43_arch_poll()
-#endif
+            // Send and receive QoS 0 message
+            sprintf(buf, "Msg nr: %d QoS 0 message", ++msg_count);
+            printf("%s\n", buf);
+            message.qos = MQTT::QOS0;
+            message.payloadlen = strlen(buf) + 1;
+            rc = client.publish(topic, message);
+            printf("Publish rc=%d\n", rc);
+        }
     }
 
-
+    cyw43_arch_poll(); // obsolete? - see below (DONT TOUCH, Chesterton's fence)
+    client.yield(100); // socket that client uses calls cyw43_arch_poll()
 }
-
